95818920c84ae1464d962de072ab9f4d38e9e892
stipe24 <51133162+stipe24@users.noreply.github.com>
2022-04-09 21:44:04 +0200

    product entity

---
+++ src/main/java/com/ingemark/productsservice/model/ProductEntity.java
@@ -0,0 +1,56 @@
+package com.ingemark.productsservice.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+import javax.validation.constraints.PositiveOrZero;
+import javax.validation.constraints.Size;
+import java.time.Instant;
+import java.util.Objects;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Entity
+@Table(name = "product")
+public class ProductEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int id;
+
+    @Size(min = 10, max = 10)
+    @Column(unique = true)
+    private String code;
+
+    private String name;
+
+    @PositiveOrZero
+    private float price;
+
+    private String description;
+
+    @Column(name = "is_available")
+    private boolean isAvailable;
+
+    @Column(name = "created_at")
+    private Instant createdAt;
+
+    @Column(name = "updated_at")
+    private Instant updatedAt;
+
+    @PrePersist
+    protected void onCreate() {
+        if (Objects.isNull(createdAt)) {
+            createdAt = Instant.now();
+        }
+    }
+
+    @PreUpdate
+    protected void onUpdate() {
+        updatedAt = Instant.now();
+    }
+
+}
