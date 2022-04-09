5cf6f0ac031f87fbd48be4769f44fc38f881b8b7
stipe24 <51133162+stipe24@users.noreply.github.com>
2022-04-09 22:36:19 +0200

    implement simple product controller

--- src/main/java/com/ingemark/productsservice/controller/ProductController.java
+++ src/main/java/com/ingemark/productsservice/controller/ProductController.java
@@ -1,5 +1,37 @@
 package com.ingemark.productsservice.controller;
 
+import com.ingemark.productsservice.model.ProductEntity;
+import com.ingemark.productsservice.service.ProductService;
+import lombok.AllArgsConstructor;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@AllArgsConstructor
+@RequestMapping("/products")
 public class ProductController {
+
+    private final ProductService productService;
+
+    @GetMapping
+    public List<ProductEntity> getProducts() {
+        return productService.get();
+    }
+
+    @PostMapping
+    public ProductEntity create(ProductEntity product) {
+        return productService.save(product);
+    }
+
+    @PatchMapping("/{id}")
+    public ProductEntity update(@PathVariable int id, ProductEntity product) {
+        return productService.save(product);
+    }
+
+    @DeleteMapping("/{id}")
+    public void delete(@PathVariable int id) {
+        productService.delete(id);
+    }
 
 }
--- src/main/java/com/ingemark/productsservice/service/ProductService.java
+++ src/main/java/com/ingemark/productsservice/service/ProductService.java
@@ -1,5 +1,37 @@
 package com.ingemark.productsservice.service;
 
+import com.ingemark.productsservice.model.ProductEntity;
+import com.ingemark.productsservice.repository.ProductRepository;
+import lombok.AllArgsConstructor;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+@AllArgsConstructor
 public class ProductService {
+
+    private final ProductRepository productRepository;
+
+    public List<ProductEntity> get() {
+        return productRepository.findAll();
+    }
+
+    public ProductEntity save(ProductEntity entity) {
+        return productRepository.save(entity);
+    }
+
+    public ProductEntity update(int id, ProductEntity product) {
+        var oldProduct = productRepository.findById(id);
+        if (oldProduct.isPresent()) {
+            //TODO: update name, price, description....
+            return productRepository.save(oldProduct.get());
+        }
+        return null;
+    }
+
+    public void delete(int id) {
+        productRepository.deleteById(id);
+    }
 
 }
